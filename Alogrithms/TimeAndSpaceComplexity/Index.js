c3bd70e6b65528ae9128640cb91bdb267274e56c
Prabin Gurung <37467008+Soltee@users.noreply.github.com>
2023-01-01 21:49:40 +0545

    Learn Time & Space Compexity

---
+++ Alogrithms/TimeAndSpaceComplexity/Index.js
@@ -0,0 +1,37 @@
+// Tools to Represet Time & Space Complexity
+// 1. Big-O Notation (o-notation) - Worst Case Complexity
+// 2. Omega-O Notation (o-notation) - Best Case Complexity
+// 3. Theta-O Notation (o-notation) - Average Case Complexity
+
+// Big-O Notation
+// Features
+// 1. Expresses on inputs
+// 2. Focus on bigger pictures rather than minute details
+// 3. Remove all constants, all less dominants terms
+ 
+//**** TIME Complexity
+// Types
+// 1. Linear O(n)
+// 	- For an string of length 10 in a loop,
+// 		- the loops iterates 10 times, each one by one
+// 2. Constants O(1)
+// 	- The code runs only once
+// 3. Quadratic O(n*n)
+// 	- Loops inside loops
+// 3. Cubic O(n*n *n)
+// 	- Loops inside loops
+// 4. Logarithmic O(logn)
+// 	- IF the inputs size reduce by half in every iteration
+// 	
+//**** SPACE Complexity
+// Types
+// 1. Linear O(n)
+// 	- The extra space grows as the input size grows
+// 2. Constants O(1)
+// 	- The Algorithms doesnot need extra memory
+// 	- The memory doesnot depend on the input size
+// 	- Eg. Sorting Alogrithms  in array withou utilizing another array 
+// 3. Quadratic O(n*n)
+// 3. Cubic O(n*n *n)
+   // 4. Logarithmic O(logn)
+// 	- Extra space needed grows but  not at the rate as inputs
