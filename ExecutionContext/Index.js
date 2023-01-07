93856a3fc4ffd3a566a5f5000360e6d1f61e36ed
Prabin Gurung <37467008+Soltee@users.noreply.github.com>
2023-01-07 23:25:54 +0545

    Javascript execution context

---
+++ ExecutionContext/Index.js
@@ -0,0 +1,8 @@
+//Global Execution Context
+//the Last will get out first
+//
+// ************* Variable Context / Memory Creation Phase
+//- Where variable and values are stored on memory heap
+// ************* Execution Context
+// - This thread execution
+// - Each line of code is executed line by line, not in parallels
